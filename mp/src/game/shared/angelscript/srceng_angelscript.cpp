c96a9a2a025bb6b6a027a7a7db38ce9f692ac7b5
Bitl <bitlblox@gmail.com>
2022-05-15 13:07:46 -0700

    initalize

---
+++ mp/src/game/shared/angelscript/srceng_angelscript.cpp
@@ -0,0 +1,87 @@
+#include "cbase.h"
+#include "srceng_angelscript.h"
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+#ifdef CLIENT_DLL
+//------------------------------------------------------------------------------
+// Purpose: Disables all NPCs
+//------------------------------------------------------------------------------
+void CC_PrintVersion(void)
+{
+	Msg("Version: %s\n", ANGELSCRIPT_VERSION_STRING);
+}
+static ConCommand as_version("as_version", CC_PrintVersion, "", FCVAR_CLIENTDLL);
+#endif
+
+#ifdef  GAME_DLL
+CAngelScript g_AngelScript_Server;
+const char* m_sName = "GAME_DLL";
+#elif	CLIENT_DLL
+CAngelScript g_AngelScript_Client;
+const char* m_sName = "CLIENT_DLL";
+#else
+const char* m_sName = "what the hell did you do";
+#endif //  GAME_DLL
+
+void MessageCallback(const asSMessageInfo* msg, void* param)
+{
+	const char* type = "ERR";
+	if (msg->type == asMSGTYPE_WARNING)
+	{
+		type = "WARN";
+		Warning("CAngelScript (%s): %s (%d, %d) : %s : %s\n", m_sName, msg->section, msg->row, msg->col, type, msg->message);
+	}
+	else if (msg->type == asMSGTYPE_INFORMATION)
+	{
+		type = "INFO";
+		Msg("CAngelScript (%s): %s (%d, %d) : %s : %s\n", m_sName, msg->section, msg->row, msg->col, type, msg->message);
+	}
+	else
+	{
+		Error("CAngelScript (%s): %s (%d, %d) : %s : %s\n", m_sName, msg->section, msg->row, msg->col, type, msg->message);
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+CAngelScript::CAngelScript()
+{
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+CAngelScript::~CAngelScript()
+{
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CAngelScript::Init()
+{
+	Msg("CAngelScript (%s): Initalizing version %s...\n", m_sName, ANGELSCRIPT_VERSION_STRING);
+	//init the engine
+	m_hScriptEngine = asCreateScriptEngine();
+
+	if (m_hScriptEngine == NULL)
+	{
+		Warning("CAngelScript (%s): Initalizion failed due to null script engine pointer...\n", m_sName);
+		return;
+	}
+	else
+	{
+		// Set the message callback to receive information on errors in human readable form.
+		int r = m_hScriptEngine->SetMessageCallback(asFUNCTION(MessageCallback), 0, asCALL_CDECL);
+		Msg("CAngelScript (%s): SetMessageCallback returned %i\n", m_sName, r);
+		Msg("CAngelScript (%s): Initalized!\n", m_sName);
+	}
+}
+
+void CAngelScript::Shutdown()
+{
+	m_hScriptEngine->ShutDownAndRelease();
+}
---
+++ mp/src/game/shared/angelscript/srceng_angelscript.h
@@ -0,0 +1,28 @@
+#ifdef _WIN32
+#pragma once
+#endif
+
+#include "../../angelscript/sdk/angelscript/include/angelscript.h"
+#include "../../angelscript/sdk/add_on/scriptstdstring/scriptstdstring.h"
+#include "../../angelscript/sdk/add_on/scriptbuilder/scriptbuilder.h"
+#include "const.h"
+
+class CAngelScript
+{
+public:
+	CAngelScript();
+	~CAngelScript();
+
+	void		Init();
+	void		Shutdown();
+	asIScriptEngine* GetScriptEngine() { return  m_hScriptEngine; }
+
+private:
+	asIScriptEngine* m_hScriptEngine;
+};
+
+#ifdef  GAME_DLL
+extern CAngelScript g_AngelScript_Server;
+#elif	CLIENT_DLL
+extern CAngelScript g_AngelScript_Client;
+#endif //  GAME_DLL
